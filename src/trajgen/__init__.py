12b8fa40258ee8e0b73bd56a0a6a0d942a9c44d5
Richard Murray <murray@cds.caltech.edu>
2013-06-09 17:07:36 +0000

    Cleaning up braches:
    * Delete old control-0.3 branch
    * Create trajgent-rmm as branch for implementing trajectory generation
    * Move rmm-trajgen to trajgen-rmm

---
+++ src/trajgen/__init__.py
@@ -0,0 +1,12 @@
+# Basis function families
+from trajgen.basis import BasisFamily
+from trajgen.poly import PolyFamily
+
+# Classes
+from trajgen.systraj import SystemTrajectory
+from trajgen.flatsys import FlatSystem
+from trajgen.linflat import LinearFlatSystem
+
+# Package functions 
+from trajgen.flatsys import point_to_point
+
